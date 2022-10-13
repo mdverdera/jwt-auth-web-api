a3d5440613e7329b45b4ed614547361494c48ffd
MadMan24 <mdverdera@yahoo.com>
2022-10-13 10:20:56 +0800

    Injecting dependency.

---
+++ JwtAuthWebAPI/JwtAuthWebAPI/Data/DataDbContext.cs
@@ -0,0 +1,17 @@
+﻿using JwtAuthWebAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthWebAPI.Data
+{
+    public class DataDbContext: DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ JwtAuthWebAPI/JwtAuthWebAPI/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthWebAPI.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
